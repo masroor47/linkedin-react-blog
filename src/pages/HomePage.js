5c34a2a021f41244702b8600f9ae46cd42d8908c
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-12-05 23:02:58 -0500

    add 'not found' page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,16 @@
 const HomePage = () => {
     return (
+        <>
         <h1>This is the home page!</h1>
+        <h2>Lor</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, minus ut. Hic asperiores temporibus itaque fugit explicabo voluptatem nobis aliquid autem beatae cum reprehenderit, id expedita, ipsa dignissimos, laboriosam earum.
+        </p>
+        <h2>Em</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, minus ut. Hic asperiores temporibus itaque fugit explicabo voluptatem nobis aliquid autem beatae cum reprehenderit, id expedita, ipsa dignissimos, laboriosam earum.
+        </p>
+        </>
     );
 }
 
